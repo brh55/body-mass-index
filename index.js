fc1aa28b5d23599b152fd6385dac8ae99456abb2
Brandon Him <brh55@drexel.edu>
2017-02-04 23:48:32 -0800

    Fix unit reference

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ module.exports = (weight, height) => {
 
 	if (typeof weight === 'string') {
 		// \d+\s?(mass units)
-		const pattern = new RegExp(`\d+\s?(${convert().possibilities('mass').join('|')})`, g);
-		const unit = weight.match(pattern)[0];
-		kg = parseInt(weight.match(/\d+/)[0], 10);
+		const units_pattern = new RegExp(`\d+\s?(${convert().possibilities('mass').join('|')})`, g);
+		const unit_pattern = new RegExp(convert().possibilities('mass').join('|'));
+		const weights = weight.match(units_pattern);
+		units = units.map(u => weight.match(unit_pattern));
+		const values = weight.match(/\d+/).filter(x => parseInt(x, 10));
 
-		if (unit !== 'kg') {
-			kg = convert(kg).from(unit).to('kg');
+		if (units.length > 1) {
+			kg = units.map((unit, index) => convert(values[index]).from(unit).to('kg'))
+					  .reduce((a, b) => a + b);
+
+		} else if (units[0] !== 'kg') {
+			kg = convert(values[0]).from(units[0]).to('kg');
+
+		} else {
+			kg = units[0];
 		}
 	}
 
